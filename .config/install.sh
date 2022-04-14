86062834a38f0a28144027bc3ec0e4e359f70550
Deottive <bhomia123456789@gmail.com>
2022-04-14 17:12:19 +0530

    Changed something

---
+++ .config/install.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+#This is a Post installing script which will change things up and fasten the proces of setting up my environment when I distrohop
+
+# Making Appropriate Folders 
+
+# Home Directory
+sudo mkdir Clone
+pwd
+cd
+pwd 
+sudo mkdir .themes
+cd
+pwd
+sudo mkdir .fonts
+cd
+# Downlaods management 
+cd
+pwd
+sudo mkdir ~/Downloads/Personal
+cd 
+pwd
+sudo mkdir ~/Downloads/College
+# Configs
+cd ~/.configs
+pwd
+sudo mkdir ~/.config/alacritty
+cd ~/.config/alacritty/
+mkdir ~/.config/alacritty/themes
+cd ~/.config/alacritty/themes
+cd ..
+pwd
+cd ..
+pwd
+sudo mkdir ~/.config/gedit
